57def422e12eee71fa9c0e0c6d93a8ed16a1b9b2
JavierBrizuela <jbrizuela1976@gmail.com>
2025-06-02 16:06:19 -0300

    fix api adress

--- static/config.js
+++ static/config.js
@@ -1,5 +1,5 @@
 // Configuracion de variables globales para la aplicacion
 window.CONFIG = {
     //API_URL: 'http://localhost:5000'
-    API_URL: 'https://fotocarnetapi.v2.javierbrizuela.dev',
+    API_URL: 'https://fotocarnetapi-v2.javierbrizuela.dev',
 };
